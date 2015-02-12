8aca7349d8be42443b5d5bf3df54a08ea2ff4861
Thomas Gelf <thomas@gelf.net>
2015-02-12 12:39:28 +0100

    configuration: menu, permissions

---
+++ configuration.php
@@ -0,0 +1,26 @@
+<?php
+
+use Icinga\Application\Config;
+
+$section = $this->menuSection($this->translate('Maps'))
+    ->setUrl('nagvis/show/map')
+    ->setIcon('globe');
+
+foreach (Config::module('nagvis')->getSection('menu') as $name => $caption) {
+    $section->add($caption)->setUrl('nagvis/show/map')->getUrl()->setParams(array('map' => $name));
+}
+
+$this->providePermission(
+    'nagvis/read',
+    $this->translate('Show NagVis maps')
+);
+
+$this->providePermission(
+    'nagvis/admin',
+    $this->translate('Nagvis administration')
+);
+
+$this->provideRestriction(
+    'nagvis/map/filter',
+    $this->translate('Filter NagVis maps')
+);

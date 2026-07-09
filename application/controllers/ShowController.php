0353c226f83f683aae50f9cd539f7dc4911e6bff
mj84 <mj84@users.noreply.github.com>
2026-07-09 10:45:35 +0200

    Implemented requested changes

--- application/controllers/ShowController.php
+++ application/controllers/ShowController.php
@@ -69,13 +69,12 @@ class ShowController extends Controller
         }
 
         $baseurl = $this->Config()->get('global', 'baseurl', '/nagvis');
-
         $url = $baseurl . '/frontend/nagvis-js/index.php';
-        $url .= '?mod=Map&act=view&show=' . urlencode($map);
 
-        if($map == 'global-overview') {
-            $url = $baseurl . '/frontend/nagvis-js/index.php';
+        if ($map === 'global-overview') {
             $url .= '?mod=Overview';
+        } else {
+            $url .= '?mod=Map&act=view&show=' . urlencode($map);
         }
 
         if ($this->params->get('showMenu')) {
--- doc/90-FAQ.md
+++ doc/90-FAQ.md
@@ -8,3 +8,8 @@ in the `nagvis.ini.php` configuration file.
 ## Map Path demo-overview.cfg doesn't exist
 
 Specify a different `default-map` in the `nagvis.ini.php` configuration file.
+
+## Use the map overview provided by NagVis
+
+You can set `default-map` to `global-overview` to use the built-in overview page from NagVis.
+This automatically only shows maps the user has access to.
